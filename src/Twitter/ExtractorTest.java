6692afa8d24d62b50e95427a95e73220996b2b3a
Student <Student@10.24.77.171>
2016-02-09 23:55:21 -0600

    finished

--- src/Twitter/ExtractorTest.java
+++ src/Twitter/ExtractorTest.java
@@ -9,11 +9,11 @@ import junit.framework.TestSuite;
 import junit.framework.Test;
 
 public class ExtractorTest extends TestCase {
-	protected Extractor extractor;
+	  protected Extractor extractor;
 
 	  public static Test suite() {
-	    Class<?>[] testClasses = { OffsetConversionTest.class,
-	            MentionTest.class, HashtagTest.class};
+	    Class<?>[] testClasses = { OffsetConversionTest.class, ReplyTest.class,
+	            MentionTest.class, HashtagTest.class, URLTest.class };
 	    return new TestSuite(testClasses);
 	  }
 
@@ -95,9 +95,39 @@ public class ExtractorTest extends TestCase {
 	        codePointOffsets.add(testData.codePointCount(0, charOffset));
 	        entities.add(new Extractor.Entity(matcher, Type.HASHTAG, 0, 0));
 	      }
+
+	      extractor.modifyIndicesFromUTF16ToToUnicode(testData, entities);
+
+	      for (int i = 0; i < entities.size(); i++) {
+	        assertEquals(codePointOffsets.get(i), entities.get(i).getStart());
+	      }
+
+	      extractor.modifyIndicesFromUnicodeToUTF16(testData, entities);
+
+	      for (int i = 0; i < entities.size(); i++) {
+	        // This assertion could fail if the entity location is in the middle
+	        // of a surrogate pair, since there is no equivalent code point
+	        // offset to that location. It would be pathological for an entity to
+	        // start at that point, so we can just let the test fail in that case.
+	        assertEquals(charOffsets.get(i), entities.get(i).getStart());
+	      }
 	    }
 	  }
 
+	  /**
+	   * Tests for the extractReplyScreenname method
+	   */
+	  public static class ReplyTest extends ExtractorTest {
+	    public void testReplyAtTheStart() {
+	      String extracted = extractor.extractReplyScreenname("@user reply");
+	      assertEquals("Failed to extract reply at the start", "user", extracted);
+	    }
+
+	    public void testReplyWithLeadingSpace() {
+	      String extracted = extractor.extractReplyScreenname(" @user reply");
+	      assertEquals("Failed to extract reply with leading space", "user", extracted);
+	    }
+	  }
 
 	  /**
 	   * Tests for the extractMentionedScreennames{WithIndices} methods
@@ -147,6 +177,22 @@ public class ExtractorTest extends TestCase {
 	      assertEquals(extracted.get(1).value, "mention");
 	      assertEquals(extracted.get(1).start, 15);
 	      assertEquals(extracted.get(1).end, 23);
+
+	      // count U+10400 as single character
+	      extractor.modifyIndicesFromUTF16ToToUnicode(text, extracted);
+	      assertEquals(extracted.size(), 2);
+	      assertEquals(extracted.get(0).start, 2);
+	      assertEquals(extracted.get(0).end, 10);
+	      assertEquals(extracted.get(1).start, 13);
+	      assertEquals(extracted.get(1).end, 21);
+
+	      // count U+10400 as 2 characters (as in UTF-16)
+	      extractor.modifyIndicesFromUnicodeToUTF16(text, extracted);
+	      assertEquals(2, extracted.size());
+	      assertEquals(3, extracted.get(0).start);
+	      assertEquals(11, extracted.get(0).end);
+	      assertEquals(15, extracted.get(1).start);
+	      assertEquals(23, extracted.get(1).end);
 	    }
 	  }
 
@@ -198,7 +244,110 @@ public class ExtractorTest extends TestCase {
 	      assertEquals(extracted.get(1).value, "hashtag");
 	      assertEquals(extracted.get(1).start, 15);
 	      assertEquals(extracted.get(1).end, 23);
+
+	      // count U+10400 as single character
+	      extractor.modifyIndicesFromUTF16ToToUnicode(text, extracted);
+	      assertEquals(extracted.size(), 2);
+	      assertEquals(extracted.get(0).start, 2);
+	      assertEquals(extracted.get(0).end, 10);
+	      assertEquals(extracted.get(1).start, 13);
+	      assertEquals(extracted.get(1).end, 21);
+
+	      // count U+10400 as 2 characters (as in UTF-16)
+	      extractor.modifyIndicesFromUnicodeToUTF16(text, extracted);
+	      assertEquals(extracted.size(), 2);
+	      assertEquals(extracted.get(0).start, 3);
+	      assertEquals(extracted.get(0).end, 11);
+	      assertEquals(extracted.get(1).start, 15);
+	      assertEquals(extracted.get(1).end, 23);
 	    }
+	  }
+
+	   /**
+	   * Tests for the extractURLsWithIndices method
+	   */
+	  public static class URLTest extends ExtractorTest {
+	   public void testUrlWithIndices() {
+	      List<Extractor.Entity> extracted = extractor.extractURLsWithIndices("http://t.co url https://www.twitter.com ");
+	      assertEquals(extracted.get(0).getStart().intValue(), 0);
+	      assertEquals(extracted.get(0).getEnd().intValue(), 11);
+	      assertEquals(extracted.get(1).getStart().intValue(), 16);
+	      assertEquals(extracted.get(1).getEnd().intValue(), 39);
+	   }
+
+	   public void testUrlWithoutProtocol() {
+	     String text = "www.twitter.com, www.yahoo.co.jp, t.co/blahblah, www.poloshirts.uk.com";
+	     assertList("Failed to extract URLs without protocol",
+	         new String[]{"www.twitter.com", "www.yahoo.co.jp", "t.co/blahblah", "www.poloshirts.uk.com"},
+	         extractor.extractURLs(text));
+
+	     List<Extractor.Entity> extracted = extractor.extractURLsWithIndices(text);
+	     assertEquals(extracted.get(0).getStart().intValue(), 0);
+	     assertEquals(extracted.get(0).getEnd().intValue(), 15);
+	     assertEquals(extracted.get(1).getStart().intValue(), 17);
+	     assertEquals(extracted.get(1).getEnd().intValue(), 32);
+	     assertEquals(extracted.get(2).getStart().intValue(), 34);
+	     assertEquals(extracted.get(2).getEnd().intValue(), 47);
+
+	     extractor.setExtractURLWithoutProtocol(false);
+	     assertTrue("Should not extract URLs w/o protocol", extractor.extractURLs(text).isEmpty());
+	   }
+
+	   public void testURLFollowedByPunctuations() {
+	     String text ="http://games.aarp.org/games/mahjongg-dimensions.aspx!!!!!!";
+	     assertList("Failed to extract URLs followed by punctuations",
+	         new String[]{"http://games.aarp.org/games/mahjongg-dimensions.aspx"},
+	         extractor.extractURLs(text));
+	   }
+
+	   public void testUrlWithPunctuation() {
+	     String[] urls = new String[] {
+	       "http://www.foo.com/foo/path-with-period./",
+	       "http://www.foo.org.za/foo/bar/688.1",
+	       "http://www.foo.com/bar-path/some.stm?param1=foo;param2=P1|0||P2|0",
+	       "http://foo.com/bar/123/foo_&_bar/",
+	       "http://foo.com/bar(test)bar(test)bar(test)",
+	       "www.foo.com/foo/path-with-period./",
+	       "www.foo.org.za/foo/bar/688.1",
+	       "www.foo.com/bar-path/some.stm?param1=foo;param2=P1|0||P2|0",
+	       "foo.com/bar/123/foo_&_bar/"
+	     };
+
+	     for (String url : urls) {
+	       assertEquals(url, extractor.extractURLs(url).get(0));
+	     }
+	   }
+
+	   public void testUrlnWithSupplementaryCharacters() {
+	     // insert U+10400 before " http://twitter.com"
+	     String text = String.format("%c http://twitter.com %c http://twitter.com", 0x00010400, 0x00010400);
+
+	     // count U+10400 as 2 characters (as in UTF-16)
+	     List<Extractor.Entity> extracted = extractor.extractURLsWithIndices(text);
+	     assertEquals(extracted.size(), 2);
+	     assertEquals(extracted.get(0).value, "http://twitter.com");
+	     assertEquals(extracted.get(0).start, 3);
+	     assertEquals(extracted.get(0).end, 21);
+	     assertEquals(extracted.get(1).value, "http://twitter.com");
+	     assertEquals(extracted.get(1).start, 25);
+	     assertEquals(extracted.get(1).end, 43);
+
+	     // count U+10400 as single character
+	     extractor.modifyIndicesFromUTF16ToToUnicode(text, extracted);
+	     assertEquals(extracted.size(), 2);
+	     assertEquals(extracted.get(0).start, 2);
+	     assertEquals(extracted.get(0).end, 20);
+	     assertEquals(extracted.get(1).start, 23);
+	     assertEquals(extracted.get(1).end, 41);
+
+	     // count U+10400 as 2 characters (as in UTF-16)
+	     extractor.modifyIndicesFromUnicodeToUTF16(text, extracted);
+	     assertEquals(extracted.size(), 2);
+	     assertEquals(extracted.get(0).start, 3);
+	     assertEquals(extracted.get(0).end, 21);
+	     assertEquals(extracted.get(1).start, 25);
+	     assertEquals(extracted.get(1).end, 43);
+	   }
 	  }
 
 	  public void testUrlWithSpecialCCTLDWithoutProtocol() {
